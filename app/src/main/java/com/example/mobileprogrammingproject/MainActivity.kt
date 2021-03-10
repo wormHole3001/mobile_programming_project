6e72f4a1f97c5815259bc50697af405d8fcc440b
Fernando Gonzalez <fgonzalez19@islander.tamucc.edu>
2021-03-10 15:21:30 -0600

    * Rough demo of how location works. Able to get long/lat coords.

--- app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
@@ -4,19 +4,39 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Checking if application has location permission. If not; request permission.
-        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
+    }
+
+    private fun fetchLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+            return
+        }
+        val tv1: TextView = findViewById(R.id.textView1)
+        task.addOnSuccessListener {
+            if (it != null) {
+                val tv1: TextView = findViewById(R.id.textView1)
+                tv1.text = "${it.latitude} -- ${it.longitude}"
+                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
