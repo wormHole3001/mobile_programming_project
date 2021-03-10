44e89d8fad2d1866c854424b0e832cd3797b8b8c
Fernando Gonzalez <fgonzalez19@islander.tamucc.edu>
2021-03-10 11:13:50 -0600

    * Added gradient background. * Created object for the API weather api key * Fixed issue with styles.xml

--- app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Weather API
+    object ApiConstant{
+        // Can be accessed like so apiConstant.WEATHER_API
+        const val WEATHER_API = "a7738a77d04912d0a247200f2a860541"
     }
 }
