5627d49a5e56a80ead10c76ce7aea5262c4b34f6
Fernando Gonzalez <fgonzalez19@islander.tamucc.edu>
2021-03-10 14:45:39 -0600

    * Corrected imports/implementations * Check for user permissions

--- app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.mobileprogrammingproject
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Checking if application has location permission. If not; request permission.
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
     }
 
     // Weather API
