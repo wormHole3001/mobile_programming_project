0707ed018af7b2485322398d70a8d9aeb68eb5be
Fernando Gonzalez <fgonzalez19@islander.tamucc.edu>
2021-03-24 13:15:57 -0500

    Recreated code format to account for the new API. Basic prototype is working now. App will display user city and current temp in celsius.

--- app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
@@ -1,53 +1,93 @@
 package com.example.mobileprogrammingproject
 
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import java.util.*
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var weatherApiUrl = ""
+    private var weatherApi = ApiConstant.WEATHER_API
+    private lateinit var currentTemp: TextView
+    private lateinit var cityName: TextView
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        fetchLocation()
+        /* Link currentTemp; Will be used to display the temperature */
+        /* Link cityName; Will be used to display the current city name */
+        currentTemp = findViewById(R.id.textView)
+        cityName = findViewById(R.id.textView1)
+        /* Create an instance of the fused location provider */
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        obtainLocation()
     }
 
-    private fun fetchLocation() {
+    private fun obtainLocation() {
         /*
-         * fetchLocation will get the last known location which is also
-         * the current location. The function will also update text on the app
-         * to display the users current location.
+         * obtainLocation() will create a string which is the api call. It will then call the
+         * getTemp() function to get the weather for the current device location
          */
-        val task = fusedLocationProviderClient.lastLocation
-        val cityName: TextView = findViewById(R.id.textView1)
 
         /* Self check for permissions. This will prompt the user to allow location access to the phone */
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
             return
         }
-        /* Listener to get the location and update the text on the app with the current city name */
-        task.addOnSuccessListener {
-            if (it != null) {
-                val geo = Geocoder(this, Locale.getDefault())
-                val addresses = geo.getFromLocation(it.latitude,it.longitude,1)
-                val locality = addresses[0].locality
-                cityName.text = locality
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                /* Create and api call string using longitude, latitude, and our provided api key */
+                weatherApiUrl = "https://api.weatherbit.io/v2.0/current?" + "lat=" + location?.latitude + "&lon=" + location?.longitude + "&key=" + weatherApi
+                getTemp()
             }
-        }
+    }
+    @SuppressLint("SetTextI18n")
+    private fun getTemp() {
+        /*
+         * getTemp() will create an API request and parse the returning JSON data.
+         * It will then use this information to display to the user different data for
+         * their location such as current city and temperature.
+         */
+
+        /* Init the RequestQueue */
+        val queue = Volley.newRequestQueue(this)
+        val url: String = weatherApiUrl
+        
+        /* Create request response */
+        val stringReq = StringRequest(Request.Method.GET, url,
+            { response ->
+                /* Create JSON Object */
+                val obj = JSONObject(response)
+                /* Get 'data' from JSON object */
+                val arr = obj.getJSONArray("data")
+                /* Get JSON data from index 0 */
+                val obj2 = arr.getJSONObject(0)
+                /* Show the user useful information by making changes to the UI */
+                currentTemp.text = obj2.getString("temp")
+                cityName.text = obj2.getString("city_name")
+                /*
+                 * TODO: Convert celsius to fahrenheit for easier readability in the US.
+                 * TODO: Add more useful information related to weather to the user.
+                 */
+            },
+            // In case of any error
+            { currentTemp.text = "Something Went Wrong" })
+        queue.add(stringReq)
     }
 
-    // Weather API
+    /* Weather API Object */
     object ApiConstant{
-        // Can be accessed like so apiConstant.WEATHER_API
-        const val WEATHER_API = "a7738a77d04912d0a247200f2a860541"
+        /* Can be accessed like so ApiConstant.WEATHER_API */
+        const val WEATHER_API = "86d9d9dde5944416b9adeb980e522099"
     }
 }
